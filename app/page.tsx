03dff0eec439c85c25507cc60ddd1a401611d37e
davidpierce24 <pie2ce@gmail.com>
2023-07-09 15:47:35 -0400

    formatting updates

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,36 @@
 
 export default function Home() {
   return (
-    <main className="">
+    <main className=" bg-gradient-to-t from-green-900 to-green-400">
       
+      {/* navbar */}
+      <div className="w-full h-20 bg-black bg-opacity-5 flex">
+
+      </div>
+
+
+      {/* body */}
+      <div className="flex flex-col h-full p-5 gap-5">
+        <div className="bg-black bg-opacity-20 h-20"></div>
+        <div className="bg-black bg-opacity-20 h-20"></div>
+        <div className="bg-black bg-opacity-20 h-20"></div>
+        <div className="bg-black bg-opacity-20 h-20"></div>
+        <div className="bg-black bg-opacity-20 h-20"></div>
+        <div className="bg-black bg-opacity-20 h-20"></div>
+        <div className="bg-black bg-opacity-20 h-20"></div>
+        <div className="bg-black bg-opacity-20 h-20"></div>
+        <div className="bg-black bg-opacity-20 h-20"></div>
+        <div className="bg-black bg-opacity-20 h-20"></div>
+        <div className="bg-black bg-opacity-20 h-20"></div>
+
+        
+      </div>
+
+      {/* footer */}
+      <div className="w-full h-60 bg-black">
+
+      </div>
+
     </main>
   )
 }
