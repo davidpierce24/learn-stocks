aabf34369f81eeffc16aa756b72f361773fea2d8
DavidPierce24 <pie2ce@gmail.com>
2023-07-18 21:27:51 -0400

    layout testing updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,82 @@
-
+import { BsPiggyBank } from "react-icons/bs";
 
 export default function Home() {
   return (
-    <main className=" bg-gradient-to-t from-green-900 to-green-400">
+    // <main className=" bg-gradient-to-t from-green-900 to-green-400  h-screen">
       
-      {/* navbar */}
-      <div className="w-full h-20 bg-black bg-opacity-5 flex">
+    //   {/* navbar */}
+    //   <div className=" w-full h-20 bg-black bg-opacity-5 p-6 flex items-center font-bold text-white text-2xl">
+    //     <div className="basis-2/6 flex items-center">
+    //       <BsPiggyBank className="w-8 h-8 mr-1" />
+    //       <span><span className="text-3xl">S</span>to<span className="text-3xl">X</span></span>
+    //     </div>
+    //     <div className="basis-2/6"></div>
+    //     <div className="basis-2/6"></div>
+    //   </div>
 
-      </div>
+
+    //   {/* Side Navbar */}
+    //   <div className="flex pt-5 text-lg font-semibold h-fit">
+    //     <div className="h-full basis-1/6  flex flex-col gap-2 items-start text-white px-5 overflow-y-auto">
+    //       <div>Stocks</div>
+    //       <div>test word</div>
+    //       <div>test word</div>
+    //       <div>test word</div>
+    //       <div>test word</div>
+    //       <div>test word</div>
+    //       <div>test word</div>
+    //       <div>test word</div>
+    //       <div>test word</div>
+    //       <div>test word</div>
+    //       <div>test word</div>
+    //       <div>test word</div>
+    //       <div>test word</div>
+    //       <div>test word</div>
+    //       <div>test word</div>
+    //       <div>test word</div>
+    //       <div>test word</div>
+    //       <div>test word</div>
+    //       <div>test word</div>
+    //       <div>test word</div>
+    //     </div>
+
+    //     {/* body */}
+    //     <div className="h-full basis-5/6 flex flex-col  px-5 pb-10 gap-5 overflow-y-auto">
+    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
+    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
+    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
+    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
+    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
+    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
+    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
+    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
+    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
+    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
+    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
+    //       {/* footer */}
+    //       <div className="w-full h-60 bg-black">
+
+    //       </div>
+    //     </div>
+    //   </div>
+      
 
 
-      {/* body */}
-      <div className="flex flex-col h-full p-5 gap-5">
-        <div className="bg-black bg-opacity-20 h-20"></div>
-        <div className="bg-black bg-opacity-20 h-20"></div>
-        <div className="bg-black bg-opacity-20 h-20"></div>
-        <div className="bg-black bg-opacity-20 h-20"></div>
-        <div className="bg-black bg-opacity-20 h-20"></div>
-        <div className="bg-black bg-opacity-20 h-20"></div>
-        <div className="bg-black bg-opacity-20 h-20"></div>
-        <div className="bg-black bg-opacity-20 h-20"></div>
-        <div className="bg-black bg-opacity-20 h-20"></div>
-        <div className="bg-black bg-opacity-20 h-20"></div>
-        <div className="bg-black bg-opacity-20 h-20"></div>
 
+    // </main>
+
+    <div className="relative h-screen bg-teal-200">
+      <nav className="fixed top-0 left-0 right-0 h-20 bg-black"></nav>
+      <div className="h-full flex  bg-red-400">
+
+        <div className="hidden sm:block w-60 bg-blue-500 rounded-r-lg flex flex-col p-5 gap-3 pt-20">
+          <div className="h-10  bg-white"></div>
+          <div className="h-10  bg-white"></div>
+          
+        </div>
         
       </div>
 
-      {/* footer */}
-      <div className="w-full h-60 bg-black">
-
-      </div>
-
-    </main>
+    </div>
   )
 }
