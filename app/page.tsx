e65a4530666c8fdc500c089d18ce2a49af803756
davidpierce24 <pie2ce@gmail.com>
2023-07-19 01:34:29 -0400

    figure out general layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,81 +2,51 @@ import { BsPiggyBank } from "react-icons/bs";
 
 export default function Home() {
   return (
-    // <main className=" bg-gradient-to-t from-green-900 to-green-400  h-screen">
-      
-    //   {/* navbar */}
-    //   <div className=" w-full h-20 bg-black bg-opacity-5 p-6 flex items-center font-bold text-white text-2xl">
-    //     <div className="basis-2/6 flex items-center">
-    //       <BsPiggyBank className="w-8 h-8 mr-1" />
-    //       <span><span className="text-3xl">S</span>to<span className="text-3xl">X</span></span>
-    //     </div>
-    //     <div className="basis-2/6"></div>
-    //     <div className="basis-2/6"></div>
-    //   </div>
+    // entire body
+    <body className="bg-gradient-to-t from-green-900 to-green-400">
 
+      {/* surrounding div */}
+      <div>
 
-    //   {/* Side Navbar */}
-    //   <div className="flex pt-5 text-lg font-semibold h-fit">
-    //     <div className="h-full basis-1/6  flex flex-col gap-2 items-start text-white px-5 overflow-y-auto">
-    //       <div>Stocks</div>
-    //       <div>test word</div>
-    //       <div>test word</div>
-    //       <div>test word</div>
-    //       <div>test word</div>
-    //       <div>test word</div>
-    //       <div>test word</div>
-    //       <div>test word</div>
-    //       <div>test word</div>
-    //       <div>test word</div>
-    //       <div>test word</div>
-    //       <div>test word</div>
-    //       <div>test word</div>
-    //       <div>test word</div>
-    //       <div>test word</div>
-    //       <div>test word</div>
-    //       <div>test word</div>
-    //       <div>test word</div>
-    //       <div>test word</div>
-    //       <div>test word</div>
-    //     </div>
-
-    //     {/* body */}
-    //     <div className="h-full basis-5/6 flex flex-col  px-5 pb-10 gap-5 overflow-y-auto">
-    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
-    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
-    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
-    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
-    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
-    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
-    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
-    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
-    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
-    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
-    //       <div className="bg-black bg-opacity-20 flex flex-col"><p>test</p> <p>test</p></div>
-    //       {/* footer */}
-    //       <div className="w-full h-60 bg-black">
-
-    //       </div>
-    //     </div>
-    //   </div>
-      
-
-
-
-    // </main>
-
-    <div className="relative h-screen bg-teal-200">
-      <nav className="fixed top-0 left-0 right-0 h-20 bg-black"></nav>
-      <div className="h-full flex  bg-red-400">
-
-        <div className="hidden sm:block w-60 bg-blue-500 rounded-r-lg flex flex-col p-5 gap-3 pt-20">
-          <div className="h-10  bg-white"></div>
-          <div className="h-10  bg-white"></div>
-          
+        {/* top nav */}
+        <div className="sticky top-0 w-full h-20 bg-black bg-opacity-5 backdrop-blur p-6 flex items-center font-bold text-white text-2xl z-40 shadow-sm">
+          <div className="basis-2/6 flex items-center">
+              <BsPiggyBank className="w-8 h-8 mr-1" />
+              <span><span className="text-3xl">S</span>to<span className="text-3xl">X</span></span>
+          </div>
+            <div className="basis-2/6"></div>
+            <div className="basis-2/6"></div>
         </div>
-        
-      </div>
 
-    </div>
+        {/* below top nav */}
+        <div>
+
+          {/* sidebar */}
+          <aside className="hidden sm:block w-40 fixed inset-0 top-[5rem] bg-black bg-opacity-5 backdrop-blur rounded-r-xl z-20 overflow-y-auto overflow-hidden flex pt-5 text-xl font-semibold shadow-sm">
+            <div className="h-full basis-1/6  flex flex-col gap-2 items-start text-white px-5 overflow-y-auto">
+              <div>Stocks</div>
+              <div>test word</div>
+              <div>test word</div>
+              <div>test word</div>
+              <div>test word</div>
+            </div>
+          </aside>
+
+          {/* main body */}
+          <div className="w-full pl-52 flex flex-col px-5 py-10 gap-5 z-20 bg-green-400">
+            <div className="bg-black bg-opacity-20 h-40 w-full rounded-lg"></div>
+            <div className="bg-black bg-opacity-20 h-40 w-full rounded-lg"></div>
+            <div className="bg-black bg-opacity-20 h-40 w-full rounded-lg"></div>
+            <div className="bg-black bg-opacity-20 h-40 w-full rounded-lg"></div>
+            <div className="bg-black bg-opacity-20 h-40 w-full rounded-lg"></div>
+            <div className="bg-black bg-opacity-20 h-40 w-full rounded-lg"></div>
+            <div className="bg-black bg-opacity-20 h-40 w-full rounded-lg"></div>
+            <div className="bg-black bg-opacity-20 h-40 w-full rounded-lg"></div>
+            <div className="bg-black bg-opacity-20 h-40 w-full rounded-lg"></div>
+            <div className="bg-black bg-opacity-20 h-40 w-full rounded-lg"></div>
+          </div>
+        </div>
+      </div>
+    </body>
   )
 }
