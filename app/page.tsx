d721fc47112ba2ab0d7acced60b5f3d60f03383c
DavidPierce24 <pie2ce@gmail.com>
2023-08-06 19:02:14 -0400

    keep the ui rolling

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <div className="h-20 w-full bg-black bg-opacity-10 outline outline-green-400 p-6 flex items-center font-bold text-white text-2xl">
           <div className="basis-2/6 flex items-center pl-10">
             <BsPiggyBank className="w-8 h-8 mr-1" />
-            <span><span className="text-3xl">S</span>to<span className="text-3xl">X</span></span>
+            <span className="text-3xl">bacon</span>
           </div>
           <div className="basis-2/6">
             <input type="search" className="w-full h-10 bg-white bg-opacity-20 rounded-xl"/>
@@ -23,10 +23,14 @@ export default function Home() {
 
           {/* sidebar */}
           <div className="h-full w-48 bg-black bg-opacity-10 rounded-tr-xl p-4 outline outline-green-400 ">
-            <div className="h-full flex flex-col py-2 gap-2 overflow-y-auto text-white font-semibold text-xl">
+            <div className="h-full flex flex-col py-2 gap-2 overflow-y-auto text-white font-semibold text-lg">
 
               <p>Welcome</p>
               <p className="underline">Stocks</p>
+              <p>Credit Cards</p>
+              <p>Credit Score</p>
+              <p>Rent</p>
+              <p>Mortgage</p>
 
             </div>
           </div>
@@ -35,14 +39,51 @@ export default function Home() {
           <div className="h-full w-full bg-black bg-opacity-10 rounded-tl-xl outline outline-green-400 flex flex-col p-10 gap-4 overflow-y-auto text-white">
 
             {/* title div */}
-            <div className="flex bg-white bg-opacity-10 flex-wrap">
+            <div className=" bg-white bg-opacity-0 font-semibold">
 
               <div className="flex basis-full justify-between">
-                <span className="text-4xl font-bold" >Stock Title</span>
-                <span className="text-3xl font-bold">320.4 <span className="text-lg">USD</span></span>
+                <span className="text-3xl font-bold" >Stock Title</span>
+                <span className="text-2xl font-bold">320.4 <span className="text-lg">USD</span></span>
               </div>
               
-              <p>Ticker Symbol</p>
+              <div className="flex justify-between">
+                <span>Ticker Symbol</span>
+                <span className="text-green-400">+40 (+30%) Today</span>
+              </div>
+
+              {/* chart */}
+              <div className="pt-5">
+                <span className="text-2xl">Stock Chart</span>
+                <div className="pt-5 px-32">
+                  <div className="h-72 w-full bg-black bg-opacity-30 rounded-xl">
+
+                  </div>
+                </div>
+              </div>
+
+              {/* Stock Summary / Info */}
+              <div className="pt-5">
+                <span className="text-2xl">Summary / Info</span>
+                <div className="grid grid-cols-3 gap-4 pt-2">
+                  <div className="flex justify-between hover:bg-black hover:bg-opacity-10">
+                    <span>Previous Close</span>
+                    <span>126.2</span>
+                  </div>
+                  <div className="flex justify-between">
+                    <span>Previous Close</span>
+                    <span>126.2</span>
+                  </div>
+                  <div className="flex justify-between">
+                    <span>Previous Close</span>
+                    <span>126.2</span>
+                  </div>
+                  <div className="flex justify-between">
+                    <span>Previous Close</span>
+                    <span>126.2</span>
+                  </div>
+                </div>
+              </div>
+
 
             </div>
 
